ff339c678eb2e8a7f6c34f37880dc911285ac977
Leo Costa <leoeduar@gmail.com>
2019-06-25 17:14:26 -0300

    Adding memoryDatabase and commands Del, Get_by_value, Get_by_key, Get_all and set

--- src/main/tcpServer.go
+++ src/main/tcpServer.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"strings"
 	"time"
 )
+
+var repository = make(map[string]string)
 
 func main() {
 
@@ -23,14 +26,74 @@ func main() {
 
 func handle(conn net.Conn) {
 	scanner := bufio.NewScanner(conn)
+	instructions :=
+		"Use commands:\n" +
+			"\t'SET key value'\n" +
+			"\t'GET_BY_KEY key'\n" +
+			"\t'GET_BY_VALUE value'\n" +
+			"\t'GET_ALL'\n" +
+			"\t'DEL key value'\n" +
+			"\t'EXIT'\n"
+	io.WriteString(conn, instructions)
+
 	for scanner.Scan() {
+		conn.SetDeadline(time.Now().Add(10 * time.Second))
+
 		text := scanner.Text()
-		fmt.Fprintf(conn, "%s\tI heard you say: %s\n", time.Now().Format(time.RFC3339), text)
-		if strings.ToUpper(text) == "SAIR" {
+		fmt.Fprintf(conn, "%s\treceived: %s\n", time.Now().Format(time.RFC3339), text)
+		parameters := strings.Fields(text)
+
+		switch strings.ToUpper(parameters[0]) {
+
+		case "GET_BY_KEY":
+			if len(parameters) != 2 {
+				fmt.Fprintf(conn, "For GET_BY_KEY - Expected command and key, for example: 'GET_BY_KEY name'\n")
+				continue
+			}
+			value := repository[parameters[1]]
+			fmt.Fprintf(conn, "%s\n", value)
+
+		case "GET_BY_VALUE":
+			if len(parameters) != 2 {
+				fmt.Fprintf(conn, "For GET_BY_VALUE - Expected command and value, for example: 'GET_BY_VALUE Léo'\n")
+				continue
+			}
+			for key := range repository {
+				if repository[key] == parameters[1] {
+					fmt.Fprintf(conn, "%s\n", key)
+				}
+			}
+
+		case "GET_ALL":
+			if len(parameters) != 1 {
+				fmt.Fprintf(conn, "For GET_ALL - Expected command, for example: 'GET_ALL'\n")
+				continue
+			}
+			if len(repository) != 0 {
+				fmt.Fprintf(conn, "%s\n", repository)
+			}
+
+		case "SET":
+			if len(parameters) != 3 {
+				fmt.Fprintf(conn, "For SET - Expected command, key and value, for example: 'SET name Léo'\n")
+				continue
+			}
+			repository[parameters[1]] = parameters[2]
+
+		case "DEL":
+			if len(parameters) != 2 {
+				fmt.Fprintf(conn, "for DEL - Expected command and key, for example: 'DEL name'\n")
+				continue
+			}
+			delete(repository, parameters[1])
+		case "EXIT":
+			fmt.Fprintf(conn, "Good bye\n")
+			conn.Close()
 			break
-		} else {
-			conn.SetDeadline(time.Now().Add(10 * time.Second))
+		default:
+			fmt.Fprintf(conn, "Invalid command \n%s", instructions)
 		}
 	}
+
 	defer conn.Close()
 }
