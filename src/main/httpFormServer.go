b0b088b7101abf4cabcda4b1b80cbf0a19f3084f
Leo Costa <leoeduar@gmail.com>
2019-06-27 17:03:17 -0300

    Using handleFunc

--- src/main/httpFormServer.go
+++ src/main/httpFormServer.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
-type handleValue int
-
-func (m handleValue) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func adminServerHttp(response http.ResponseWriter, request *http.Request) {
 	_ = request.ParseForm()
-	fmt.Fprintf(writer, "Form with values: %s", request.Form.Encode())
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	response.Header().Set("User-Type", "admin")
+	fmt.Fprintf(response, "Save Admin user with values: %s", request.Form.Encode())
+}
+
+func userServerHttp(response http.ResponseWriter, request *http.Request) {
+	_ = request.ParseForm()
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	response.Header().Set("User-Type", "user")
+	fmt.Fprintf(response, "Save simple user with values: %s", request.Form.Encode())
 }
 
 func main() {
-	var varHandle handleValue
-	http.ListenAndServe(":8081", varHandle)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/user", userServerHttp)
+	mux.HandleFunc("/admin", adminServerHttp)
+
+	http.ListenAndServe(":8081", mux)
 }
