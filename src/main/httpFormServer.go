df7545b9669dd547477aa45db82d1fa3920e8fec
Leo Costa <leoeduar@gmail.com>
2019-06-28 23:48:33 -0300

    Serving files

--- src/main/httpFormServer.go
+++ src/main/httpFormServer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func adminServerHttp(response http.ResponseWriter, request *http.Request) {
@@ -19,10 +20,25 @@ func userServerHttp(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "Save simple user with values: %s", request.Form.Encode())
 }
 
+func faviIconServerHttp(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	file, err := os.Open("/favicon.ico")
+	if err != nil {
+		http.Error(response, "File favicon not found", 404)
+		return
+	}
+	defer file.Close()
+
+	info, _ := file.Stat()
+
+	http.ServeContent(response, request, file.Name(), info.ModTime(), file)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/user", userServerHttp)
 	mux.HandleFunc("/admin", adminServerHttp)
+	mux.HandleFunc("/icon", faviIconServerHttp)
 
 	http.ListenAndServe(":8081", mux)
 }
